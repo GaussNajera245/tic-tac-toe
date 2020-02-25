084d16e90785c55a9ebabf208c7a993a23b459c4
GusNajera <gvt_a9@hotmail.com>
2020-02-25 14:38:11 -0600

    working

--- src/index.js
+++ src/index.js
@@ -1,12 +1,139 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function Square(props) { //Now is a function component
+    //CHECK THE ARROW FUNCTION CON PROPS ??????????????????
+    return (
+        <button className="square"  onClick={ ()=> {props.onClick()} }  > 
+            {props.value}
+        </button>
+    )
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class Board extends React.Component{
+    renderSquare(i){
+        return (
+            <Square 
+                value={this.props.squares[i]} 
+                onClick = { () => {  this.props.onClick(i);  }}
+            />
+        );
+    }
+
+    render(){
+        return(
+            <div>
+                <div className= "board-row">
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                </div>
+                <div className= "board-row">
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+                </div>
+                <div className= "board-row">
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
+                </div>
+            </div>
+        );
+    }
+}
+
+
+class Game extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            history:[{ 
+                squares: Array(9).fill(null) 
+            }],
+            xNext: true
+        };
+    }
+
+    handleClick(i){
+        const history = this.state.history;
+        const current = history[history.length - 1];
+        const squares = current.squares.slice();
+        if( calculateWinner(squares) || squares[i] ){
+            return;    // if theres something, return;  pero esto es del diablo :=0
+        }
+        squares[i] = this.state.xNext? 'X': 'O' ;
+        this.setState({ 
+            history: history.concat([{
+                squares: squares,
+            }]),
+            xNext: !this.state.xNext 
+        });
+    }
+
+    render(){
+        const history = this.state.history;
+        const current = history[history.length - 1];
+        const winner = calculateWinner(current.squares);
+        
+        let status;
+
+        winner ? 
+            status = `Winner is ${winner}`:
+            status = `Next player: ${this.state.xNext? 'X': 'O'}`;
+
+        // if(winner){
+        //     status = `Winner is ${winner}`;
+        // }
+        // else{
+        //     status = `Next player: ${this.state.xNext? 'X': 'O'}`;
+        // }
+
+        
+
+        return (
+            <div className="game">
+                <div className="game-board">
+                    <Board   
+                        squares={current.squares} 
+                        onClick = {(i) => this.handleClick(i)}
+                    />
+                </div>
+                <div className="game-info">
+                    <div>{status}</div>
+                    <ol>{/*move*/}</ol>
+                </div>
+            </div>
+        );
+    }
+}
+
+function calculateWinner(squares){
+    let r = null ;
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+    ];
+
+    lines.forEach( (element) => {
+        const [a,b,c] = element;
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c] ) {
+            r = squares[a];
+        }
+    })
+
+    return r
+}
+
+
+ReactDOM.render(
+    <Game />,
+    document.getElementById('root')
+);
